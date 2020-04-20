26cc6be01b2aa977932a0c22e2c1aef3e6ab45f6
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-04-19 23:36:12 -0700

    Use express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,12 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as express from "express";
+import * as cors from "cors";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.use(cors({ origin: true }));
+
+app.get("/", (_, res) => res.status(200).send("ok"));
+app.post("/", (_, res) => res.status(200).send("ok"));
+
+exports.app = functions.https.onRequest(app);
